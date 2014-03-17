89433dba15955516b406443afd10ba44a3536c15
kazimad <kazimad.kr@gmail.com>
2014-03-17 23:20:42 +0200

    new way

--- src/Download.java
+++ src/Download.java
@@ -1,47 +1,57 @@
 /**
  * Created by KaZimad on 17.03.14.
  */
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
+
 import java.io.*;
-import java.net.MalformedURLException;
+import java.net.SocketException;
 import java.net.URL;
+import java.net.UnknownHostException;
 
 public class Download {
     public static void main (String [] agrs) throws IOException {
         String StringLink;
         int countNameFiles = 1;
+        int countLinks = 1;
         BufferedReader br = new BufferedReader(new FileReader("links.txt"));                         // reads links from file
 
         while ((StringLink = br.readLine())!=null){
 
-//            File writeFile = new File("newfile" + countNameFiles++ + ".jpg");
-//            if (writeFile.createNewFile()){
-//                System.out.println("File is created");
-//            }
-            String link = StringLink;
-            String outFile = "newfile_" + countNameFiles++ +".png";
-            saveImage(link, outFile);
 
-        }
+        String outFile = ("newfile_" + countNameFiles++ + ".jpg");
+        saveImage(StringLink, outFile,countLinks);
+        countLinks++;
+
+         }
+        System.out.println("all done!");
     }
 
-    private static void saveImage(String link, String outFile) throws IOException {
+    private static void saveImage(String link, String outFile, int countLinks) throws IOException {
 
+        try {
         URL url = new URL(link);
         InputStream is = url.openStream();
         OutputStream os = new FileOutputStream(outFile);
 
-        byte[] b = new byte[2048];
+
         int length;
 
-        while ((length = is.read(b)) != -1) {
+        while ((length = is.read()) != -1) {
             os.write(length);
         }
 
         is.close();
         os.close();
-        System.out.println("File is written");
+        System.out.println("File # " + countLinks + " is written");
+        }
+        catch (UnknownHostException q){
+            System.out.println("link #" + countLinks+ " is unavailable" );
+
+        }
+        catch (SocketException qq) {
+            System.out.println("link #" + countLinks+ " is unavailable" );
+    }
+        catch (Exception qqq)
+        { System.out.println("link #" + countLinks+ " is unavailable" );}
     }
 }
 ///////
