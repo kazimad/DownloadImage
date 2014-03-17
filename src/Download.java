35bc0d27f4b65205aee3cbdfa4933912f70e154e
kazimad <kazimad.kr@gmail.com>
2014-03-17 22:08:23 +0200

    new way

--- src/Download.java
+++ src/Download.java
@@ -4,6 +4,7 @@
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Download {
@@ -12,18 +13,51 @@ public class Download {
         BufferedReader br = new BufferedReader(new FileReader("links.txt"));                         // reads links from file
 
         while ((StringLink = br.readLine())!=null){
-
-            BufferedImage value = null;
-            URL link = new URL(StringLink);
-
-            value = ImageIO.read(link);
             File writeFile = new File("newfile.jpg");
             if (writeFile.createNewFile()){
                 System.out.println("File is created");
             }
-            ImageIO.write(value,"jpg",writeFile);
-            System.out.println("File is written");
+            String link = StringLink;
+            String outFile = "newfile.jpg";
+            saveImage(link, outFile);
+
+        }
+    }
+
+    private static void saveImage(String link, String outFile) throws IOException {
+
+        URL url = new URL(link);
+        InputStream is = url.openStream();
+        OutputStream os = new FileOutputStream(outFile);
+
+        byte[] b = new byte[2048];
+        int length;
+
+        while ((length = is.read(b)) != -1) {
+            os.write(length);
         }
 
+        is.close();
+        os.close();
+        System.out.println("File is written");
     }
-}////////
+}
+
+
+
+
+
+//    }
+//            BufferedReader brlink = new BufferedReader(new InputStreamReader(link.openStream()));
+
+//    BufferedImage value = null;
+//    URL link = new URL(StringLink);
+//
+//    value = ImageIO.read(link);
+//    File writeFile = new File("newfile.jpg");
+//    if (writeFile.createNewFile()){
+//        System.out.println("File is created");
+//    }
+//    ImageIO.write(value,"jpg",writeFile);
+//    System.out.println("File is written");
+//}
