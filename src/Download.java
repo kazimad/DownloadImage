64c069c2707e94610171d2628b7420d7ece74a0f
kazimad <kazimad.kr@gmail.com>
2014-03-18 23:20:09 +0200

    threads

--- src/Download.java
+++ src/Download.java
@@ -8,24 +8,49 @@ import java.net.URL;
 import java.net.UnknownHostException;
 
 public class Download {
+    static Threads myThreadObject1;
+    static Threads myThreadObject2;
+    static Threads myThreadObject3;
+    static Threads myThreadObject4;
+
     public static void main (String [] agrs) throws IOException {
+        myThreadObject1 = new Threads();
+        myThreadObject2 = new Threads();
+        myThreadObject3 = new Threads();
+        myThreadObject4 = new Threads();
+        Thread myThread1 = new Thread(myThreadObject1);
+        Thread myThread2 = new Thread(myThreadObject2);
+        Thread myThread3 = new Thread(myThreadObject3);
+        Thread myThread4 = new Thread(myThreadObject4);
+
+
         String StringLink;
         int countNameFiles = 1;
         int countLinks = 1;
+        long start  = System.currentTimeMillis();
+
         BufferedReader br = new BufferedReader(new FileReader("links.txt"));                         // reads links from file
 
-        while ((StringLink = br.readLine())!=null){
+        myThread1.start();
+        myThread2.start();
+        myThread3.start();
+        myThread4.start();
 
+        while ((StringLink = br.readLine())!=null){
 
             String outFile = ("newfile_" + countNameFiles++ + ".jpg");
             saveImage(StringLink, outFile,countLinks);
             countLinks++;
 
         }
+
         System.out.println("all done!");
+        long finish = System.currentTimeMillis();
+        System.out.println(finish - start);
+
     }
 
-    private static void saveImage(String link, String outFile, int countLinks) throws IOException {
+    protected static void saveImage(String link, String outFile, int countLinks) throws IOException {
 
         try {
             URL url = new URL(link);
@@ -50,9 +75,12 @@ public class Download {
         catch (SocketException qq) {
             System.out.println("link #" + countLinks+ " is unavailable" );
         }
-        catch (Exception qqq)
-        { System.out.println("link #" + countLinks+ " is unavailable" );}
+        catch (Exception qqq){
+            System.out.println("link #" + countLinks+ " is unavailable" );
+        }
     }
+
+
 }
 //////
 
---
+++ src/Threads.java
@@ -0,0 +1,15 @@
+/**
+ * Created by KaZimad on 18.03.14.
+ */
+
+public class Threads implements Runnable{
+
+    @Override
+    public void run() {
+        Download.saveImage(StringLink );
+
+
+    }
+
+
+}
